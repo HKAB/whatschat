d92127c7c9e1ae6388f711397585b60aabc5b50b
root <root@localhost.localdomain>
2018-04-23 12:44:54 +0700

    final

--- whatschat/settings.py
+++ whatschat/settings.py
@@ -134,3 +134,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Celery settings
+CELERY_TASK_ALWAYS_EAGER = False
+
+# notifications settings
+NOTIFICATIONS_CHANNELS = {
+   'websocket': 'chat.channels.BroadCastWebSocketChannel'
+}
